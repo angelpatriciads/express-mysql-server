cdbe8c55b7839f269900cc3ae3ef266d6e88b6c6
Angelica Patricia Djaya Saputra <121279021+angelpatriciads@users.noreply.github.com>
2023-10-04 10:53:56 +0700

    Add env

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,15 @@
+require("dotenv").config();
+
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  }
+);
 
 module.exports = sequelize;
