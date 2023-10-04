d5c5591022ffec4a9a4b14c97ef167fc860e6a5b
Angelica Patricia Djaya Saputra <121279021+angelpatriciads@users.noreply.github.com>
2023-10-04 10:50:05 +0700

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("inhaler", "angel", "password", {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  protocol: "postgres",
 });
 
 module.exports = sequelize;
