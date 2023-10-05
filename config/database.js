8b780e1a56a5b6c6da3ad96d23fa98a6100e638f
Angelica Patricia Djaya Saputra <121279021+angelpatriciads@users.noreply.github.com>
2023-10-05 08:33:36 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,22 +1,13 @@
 const { Sequelize } = require("sequelize");
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
-const { DB_NAME, DB_PASSWORD, DB_USER, DB_URI } = process.env;
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASS,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: process.env.DB_DIALECT,
-//   }
-// );
-const sequelize = new Sequelize(DB_URI, {
-  define: {
-    timestamps: false,
-  },
+const sequelize = new Sequelize({
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  dialect: "mysql",
 });
 
 module.exports = sequelize;
